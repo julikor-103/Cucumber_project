50f91012d584cc66f0474d2b13a43007813629d3
julikor103 <julikor103@gmail.com>
2020-06-18 09:56:17 +0300

    StepDefinitionsModifay

--- src/test/java/hellocucumber/steps/SearchByKeyWordStepDefinitions.java
+++ src/test/java/hellocucumber/steps/SearchByKeyWordStepDefinitions.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 import static hellocucumber.steps.Hooks.*;
 import static hellocucumber.steps.Hooks.googleSearchPage;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class SearchByKeyWordStepDefinitions {
 
@@ -39,8 +41,17 @@ public class SearchByKeyWordStepDefinitions {
      @Then("Display menu items")
      public void displayMenuItems(){
       List<WebElement> menuItems = getDriver().findElements(By.cssSelector(Waiters.ListItemsForClothes));
-      Assert.assertEquals(menuItems.size(), 2);
+      Assert.assertEquals(menuItems.size(), 6);
         }
+
+    @And("Display menu items")
+    public void displayMenuItemsSecond(){
+        List<WebElement> menuItems = getDriver().findElements(By.cssSelector(Waiters.ListItemsForClothes));
+       assertThat(menuItems,hasSize(6));
+       assertThat(menuItems.size(),greaterThan(5));
+       assertThat(menuItems, contains("dress"));
+       assertThat(menuItems, allOf(notNullValue(), not(empty())));
+    }
     }
 
 
